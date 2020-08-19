f454eafd050fd9553a29980a355c247306beb2e2
zachard <arieszpzeng@yeah.net>
2020-08-19 15:11:58 +0800

    finish: 数字练习

---
+++ hello_python/chapter02/number_float.py
@@ -0,0 +1,15 @@
+# 浮点数运算
+add = 0.1 + 0.1
+print('0.1 + 0.1 = ' + str(add))
+multiply = 0.1 * 2
+print('0.1 * 2 = ' + str(multiply))
+divide = 3 / 2
+print('3 / 2 = ' + str(divide))
+two_divide_three = 2 / 3
+print('2 / 3 = ' + str(two_divide_three))
+
+# 一些特殊情况
+other_add = 0.2 + 0.1
+print('0.2 + 0.1 = ' + str(other_add))
+other_multiply = 3 * 0.1
+print('3 * 0.1 = ' + str(other_multiply))
