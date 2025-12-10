1f9a22a997206cb5a9cb9918514d4d943ac14506
Richard <arieszpzeng@yeah.net>
2025-12-11 00:12:39 +0800

    finish: 第三章鹦鹉学舌

---
+++ python-for-beginners/03_JupyterLab/BK1_CH03_03.py
@@ -0,0 +1,233 @@
+#%% [markdown]
+# Chapter 03
+## LaTeX公式  
+# Book_1《编程不难》 | 鸢尾花书：从加减乘除到机器学习  
+
+# %% [markdown]
+# $$A_{m\times n} = 
+#  \begin{bmatrix}
+#   a_{1,1} & a_{1,2} & \cdots & a_{1,n} \\
+#   a_{2,1} & a_{2,2} & \cdots & a_{2,n} \\
+#   \vdots  & \vdots  & \ddots & \vdots  \\
+#   a_{m,1} & a_{m,2} & \cdots & a_{m,n} 
+#  \end{bmatrix}$$
+
+# %% [markdown]
+# $$f_X(x)={\frac {1}{\sigma {\sqrt {2\pi }}}}
+# \exp \left({-{\frac {1}{2}}
+# \left({\frac {x-\mu }{\sigma}}\right)^{2}}\right)$$
+
+# %% [markdown]
+# $ {AaBbCc} $
+
+# %% [markdown]
+# $ \mathrm {AaBbCc} $
+
+# %% [markdown]
+# $ \mathbf {AaBbCc} $
+
+# %% [markdown]
+# $ \boldsymbol {AaBbCc} $
+
+# %% [markdown]
+# $ \mathtt {AaBbCc} $
+
+# %% [markdown]
+# $ \mathcal {ABCDEF} $
+
+# %% [markdown]
+# $ \mathbb {CRQZN} $
+
+# %% [markdown]
+# $\text {Aa Bb Cc}$
+
+# %% [markdown]
+# $\mathrm{d}x$
+
+# %% [markdown]
+# $\operatorname{T}$
+
+# %% [markdown]
+# $x'$
+
+# %% [markdown]
+# $x^{\prime}$
+
+# %% [markdown]
+# $x'’$
+
+# %% [markdown]
+# $\overrightarrow{AB}$
+
+# %% [markdown]
+#$\underline{x}$
+
+# %% [markdown]
+# $\hat{x}$
+
+# %% [markdown]
+# $\bar{x}$
+
+# %% [markdown]
+# $\dot{x}$
+
+# %% [markdown]
+# $\tilde{x}$
+
+# %% [markdown]
+# $x_i$
+
+# %% [markdown]
+# $x^i$
+
+# %% [markdown]
+# $\ddot{x}$
+
+# %% [markdown]
+# $x^*$
+
+# %% [markdown]
+# $x\dagger$
+
+# %% [markdown]
+# $x\ddagger$
+
+# %% [markdown]
+# ${\color{red}x}$
+
+# %% [markdown]
+# $x^{2}-y^{2} = \left(x+y\right)\left(x-y\right)$
+
+# %% [markdown]
+# $a_{n}x^{n}+a_{n-1}x^{n-1}+\dotsb + a_{2}x^{2} + a_{1}x + a_{0}$
+
+# %% [markdown]
+# $\sum_{k=0}^{n}a_{k}x^{k}$
+
+# %% [markdown]
+# $ ax^{2}+bx+c=0\ (a\neq 0) $
+
+# %% [markdown]
+# ${\sqrt[{n}]{a^{m}}}=(a^{m})^{1/n}=a^{m/n}=(a^{1/n})^{m}=({\sqrt[{n}]{a}})^{m}$
+
+# %% [markdown]
+# $\left({\sqrt {1-x^{2}}}\right)^{2}$
+
+# %% [markdown]
+# $\frac {1}{x+1}+{\frac {1}{x-1}}={\frac {2x}{x^{2}-1}}$
+
+# %% [markdown]
+#$x_{1,2}={\frac {-b\pm {\sqrt {b^{2}-4ac}}}{2a}}$
+
+# %% [markdown]
+# $f(x)=ax^{2}+bx+c~~{\text{ with }}~~a,b,c\in \mathbb {R} ,\ a\neq 0$
+
+# %% [markdown]
+# $f(x_1, x_2) = x_1^2 + x_2^2 + 2x_1x_2$
+
+# %% [markdown]
+# $\log_{b}(xy)=\log_{b}x+\log_{b}y$
+
+# %% [markdown]
+# $\ln(xy)=\ln x+\ln y{\text{  for  }} x>0 {\text{  and  }} y>0$
+
+# %% [markdown]
+# $f(x)=a\exp \left(-{\frac {(x-b)^{2}}{2c^{2}}}\right)$
+
+# %% [markdown]
+# $\sin ^{2}\theta +\cos ^{2}\theta =1$
+
+# %% [markdown]
+# $\sin 2\theta =2\sin \theta \cos \theta$
+
+# %% [markdown]
+# $\sin(\alpha \pm \beta )=\sin \alpha \cos \beta \pm \cos \alpha \sin \beta$
+
+# %% [markdown]
+# $\tan(\alpha \pm \beta )=\frac {\tan \alpha \pm \tan \beta }{1\mp \tan \alpha \tan \beta }$
+
+# %% [markdown]
+# $\exp(x)=\sum _{k=0}^{\infty }{\frac {x^{k}}{k!}}=1+x+{\frac {x^{2}}{2}}+{\frac {x^{3}}{6}}+{\frac {x^{4}}{24}}+\cdots $
+
+# %% [markdown]
+# $ \left(\sum _{i=0}^{n}a_{i}\right)\left(\sum _{j=0}^{n}b_{j}\right)=\sum _{i=0}^{n}\sum _{j=0}^{n}a_{i}b_{j}$
+
+# %% [markdown]
+# $\exp(x) =\lim _{n\to \infty }\left(1+{\frac {x}{n}}\right)^{n}$
+
+# %% [markdown]
+# $\frac {\mathrm{d}}{\mathrm{d}x} \exp(f(x)) =f'(x) \exp(f(x))$
+
+# %% [markdown]
+#$\int_{a}^{b}f(x) \mathrm {d} x$
+
+# %% [markdown]
+# $\int _{-\infty }^{\infty }\exp(-x^{2})\mathrm{d}x={\sqrt {\mathrm{\pi} }}$
+
+# %% [markdown]
+# $\int _{-\infty }^{\infty }\int _{-\infty }^{\infty } \exp \left({-\left(x^{2}+y^{2}\right)} \right) {\mathrm{d}x} {\mathrm{d}y} = \pi$
+
+# %% [markdown]
+# $\frac {\partial ^{2}f}{\partial x^{2}}=f''_{xx}=\partial _{xx}f=\partial _{x}^{2}f$
+
+# %% [markdown]
+# ${\frac {\partial ^{2}f}{\partial y \partial x}}={\frac {\partial }{\partial y}}\left({\frac {\partial f}{\partial x}}\right)=f''_{xy}$
+
+# %% [markdown]
+# $\mathbf {a} = {\begin{bmatrix} a_{1} \\ a_{2} \\ a_{3} \end{bmatrix}} = [a_{1}\ a_{2}\ a_{3}]^{\operatorname {T} }$
+
+# %% [markdown]
+# $\left\|\mathbf {a} \right\|=\sqrt {a_{1}^{2}+a_{2}^{2}+a_{3}^{2}}$
+
+# %% [markdown]
+# $\mathbf {a} \cdot \mathbf {b} = a_{1}b_{1} + a_{2}b_{2} + a_{3}b_{3}$
+
+# %% [markdown]
+# $\mathbf {a} \cdot \mathbf {b} =\left\|\mathbf {a} \right\|\left\|\mathbf {b} \right\|\cos \theta $
+
+# %% [markdown]
+# $\|\mathbf {x} \|_{p}=\left(\sum _{i=1}^{n}\left|x_{i}\right|^{p}\right)^{1/p}$
+
+# %% [markdown]
+# $\mathbf {A} = {\begin{bmatrix} 1 & 2\\ 3 & 4 \\ 5 & 6 \end{bmatrix}}$
+
+# %% [markdown]
+# $\mathbf {A} ={\begin{bmatrix}a_{11}&a_{12}&\cdots &a_{1n}\\a_{21}&a_{22}&\cdots &a_{2n}\\\vdots &\vdots &\ddots &\vdots \\a_{m1}&a_{m2}&\cdots &a_{mn}\end{bmatrix}}$
+
+# %% [markdown]
+# $\left(\mathbf {A} +\mathbf {B} \right)^{\operatorname {T} }=\mathbf {A} ^{\operatorname {T} }+\mathbf {B} ^{\operatorname {T} }$
+
+# %% [markdown]
+# $\left(\mathbf {AB} \right)^{\operatorname {T} }=\mathbf {B} ^{\operatorname {T} }\mathbf {A} ^{\operatorname {T} }$
+
+# %% [markdown]
+# $ \left(\mathbf {A} ^{\operatorname {T} }\right)^{-1}=\left(\mathbf {A} ^{-1}\right)^{\operatorname {T} }$
+
+# %% [markdown]
+# $\mathbf {u} \otimes \mathbf {v} = \mathbf {u} \mathbf {v} ^ {\operatorname {T}} = {\begin{bmatrix}u_{1} \\ u_{2} \\ u_{3} \\ u_{4} \end{bmatrix}} {\begin{bmatrix} v_{1}&v_{2}&v_{3} \end{bmatrix}} = {\begin{bmatrix} u_{1}v_{1} & u_{1}v_{2} & u_{1}v_{3} \\ u_{2}v_{1} & u_{2}v_{2} & u_{2}v_{3} \\ u_{3}v_{1} & u_{3}v_{2} & u_{3}v_{3} \\ u_{4}v_{1} & u_{4}v_{2} & u_{4}v_{3} \end{bmatrix}}$
+
+# %% [markdown]
+# $\det {\begin{bmatrix} a & b \\ c & d \end{bmatrix}} = ad-bc$
+
+# %% [markdown]
+# $\Pr(A\vert B)={\frac {\Pr(B\vert A)\Pr(A)}{\Pr(B)}}$
+
+# %% [markdown]
+# $ f_{X\vert Y=y}(x)={\frac {f_{X,Y}(x,y)}{f_{Y}(y)}}$
+
+# %% [markdown]
+# $\operatorname {var} (X) = \operatorname {E} \left[X^{2}\right]-\operatorname {E} [X]^{2}$
+
+# %% [markdown]
+# $\operatorname {var} (aX+bY)=a^{2}\operatorname {var} (X) + b^{2}\operatorname {var} (Y) + 2ab \operatorname {cov} (X,Y)$
+
+# %% [markdown]
+# $\operatorname {E} [X]=\int _{-\infty }^{\infty }xf_{X}(x) \operatorname {d}x$
+
+# %% [markdown]
+# $ X\sim N(\mu ,\sigma ^{2})$
+
+# %% [markdown]
+# $\frac {\exp \left(-{\frac {1}{2}}\left({\mathbf {x} }-{\boldsymbol {\mu }}\right)^{\mathrm {T} }{\boldsymbol {\Sigma }}^{-1}\left({\mathbf {x} }-{\boldsymbol {\mu }}\right)\right)}{\sqrt {(2\pi )^{k}|{\boldsymbol {\Sigma }}|}}$
+
+# %%
